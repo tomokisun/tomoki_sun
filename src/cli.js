1771bbc8dcace215345e2aeaa65cb547ad8868e8
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-11 21:51:11 +0900

    Add Fiels

---
+++ src/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict'
+const path = require('path')
+const meow = require('meow')
+const termImg = require('term-img')
+const terminalImage = require('terminal-image')
+const importJsx = require('import-jsx')
+const { h, render } = require('ink')
+
+const ui = importJsx('./ui')
+
+meow(`
+	Usage
+	  $ tomoki_sun
+`)
+
+render(h(ui))
---
+++ src/ui.js
@@ -0,0 +1,49 @@
+'use strict'
+const { h, Text } = require('ink')
+const SelectInput = require('ink-select-input')
+const opn = require('opn')
+
+const open = url => opn(url, { wait: false })
+
+const handleSelect = item => {
+  if (item.url) {
+    open(item.url)
+  }
+
+  if (item.action) {
+    item.action()
+  }
+}
+
+const items = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/tomoki_sun',
+  },
+  {
+    label: 'Qiita',
+    url: 'https://github.com/tomoki69386',
+  },
+  {
+    label: 'GitHub',
+    url: 'https://github.com/tomoki69386',
+  },
+  {
+    label: 'Quit',
+    action() {
+      process.exit()
+    },
+  },
+]
+
+module.exports = () => (
+  <div>
+    <br />
+    <div>
+      <Text>デザインできないiOSエンジニア</Text>
+      <br />
+    </div>
+    <br />
+    <SelectInput items={items} onSelect={handleSelect} />
+  </div>
+)
